2fae75cf4090a240f67efd958dc2f0fc084864d3
leoshiang <leoshiang@gmail.com>
2023-04-08 08:18:02 +0800

    加入矩陣。

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const 位元陣列 = require('./src/位元陣列')
-const 向量 = require('./src/向量')
 const 型別 = require('./src/型別')
 const 字串工具 = require('./src/字串工具')
-const {
-    索引超出範圍錯誤,
-    型別錯誤,
-    參數錯誤
-} = require('./src/例外')
+const 位元陣列 = require('./src/位元陣列')
+const 向量 = require('./src/向量')
+const 矩陣 = require('./src/矩陣')
+const 例外 = require('./src/例外')
 
 module.exports = {
     位元陣列,
+    矩陣,
     向量,
     型別,
     字串工具,
-    索引超出範圍錯誤,
-    型別錯誤,
-    參數錯誤
+    例外
 }
