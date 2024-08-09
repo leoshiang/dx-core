e80b4a83e0c10228c46079ff0273d6d582f227bd
Leo Shiang <leoshiang@gmail.com>
2024-08-09 17:21:37 +0800

    Add Telegram message sending feature
    Introduced a new module to send messages via Telegram. Updated the documentation and main index file to reflect this new capability.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const 位元陣列 = require('./src/位元陣列')
 const 向量 = require('./src/向量')
 const 矩陣 = require('./src/矩陣')
 const 檔案工具 = require('./src/檔案工具')
+const 地理資訊系統 = require('./src/地理資訊系統')
+const 通訊軟體 = require('./src/通訊軟體')
 
 module.exports = {
     位元陣列,
@@ -13,5 +15,7 @@ module.exports = {
     型別,
     字串工具,
     檔案工具,
-    例外
+    例外,
+    地理資訊系統,
+    通訊軟體
 }
---
+++ src/通訊軟體.js
@@ -0,0 +1,16 @@
+const fetch = require('sync-fetch')
+
+/**
+ *
+ * @param {string} token Telegram Bot Token
+ * @param {string} chatId Telegram Chat Id
+ * @param {string} message 要傳送的訊息
+ */
+function 傳送Telegram訊息 (token, chatId, message) {
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatid}&text=${message}`
+    fetch(url)
+}
+
+module.exports = {
+    傳送Telegram訊息
+}
