fc9544a82e30be810224f4877f234cd07d3f2a1e
Leo Shiang <leoshiang@gmail.com>
2024-12-28 09:06:24 +0800

    調整模組順序並新增日期工具
    => 重新排列模組引入及輸出順序以提升可讀性。
    => 新增 `日期工具` 模組以擴展功能。
    => 修正部分語法格式，統一分號使用。

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const 型別 = require('./src/型別')
-const 字串工具 = require('./src/字串工具')
-const 例外 = require('./src/例外')
-const 位元陣列 = require('./src/位元陣列')
-const 向量 = require('./src/向量')
-const 矩陣 = require('./src/矩陣')
-const 檔案工具 = require('./src/檔案工具')
-const 地理資訊系統 = require('./src/地理資訊系統')
-const 通訊軟體 = require('./src/通訊軟體')
+const 位元陣列 = require('./src/位元陣列');
+const 例外 = require('./src/例外');
+const 向量 = require('./src/向量');
+const 地理資訊系統 = require('./src/地理資訊系統');
+const 型別 = require('./src/型別');
+const 字串工具 = require('./src/字串工具');
+const 日期工具 = require('./src/日期工具');
+const 檔案工具 = require('./src/檔案工具');
+const 矩陣 = require('./src/矩陣');
+const 通訊軟體 = require('./src/通訊軟體');
 
 module.exports = {
-    位元陣列,
-    矩陣,
-    向量,
-    型別,
-    字串工具,
-    檔案工具,
-    例外,
-    地理資訊系統,
-    通訊軟體
-}
+	位元陣列,
+	例外,
+	向量,
+	地理資訊系統,
+	型別,
+	字串工具,
+	日期工具,
+	檔案工具,
+	矩陣,
+	通訊軟體,
+};
